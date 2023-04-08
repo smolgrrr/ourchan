8c88122c1e75f2c816f3473bc77c362126a3974b
smolgrrr <104137257+smolgrrr@users.noreply.github.com>
2023-04-08 17:10:44 +1000

    need to refactor posting events

--- src/constants/Const.ts
+++ src/constants/Const.ts
@@ -8,7 +8,7 @@ export const relayUrls = [
 // Boards
 // format is [boardlinkParam, boardPubkeyHash, boardNickname]
 export const boards = [
-  ["test", "03ccdd56313a8f4fc99f44b6b180628b22dab9d86f471a959724a0de581ca6ba", "test"],
+  // ["test", "03ccdd56313a8f4fc99f44b6b180628b22dab9d86f471a959724a0de581ca6ba", "test"],
   ["g", "fc79f7f5306b6cfe949ac5bafffafe9bb9c60c2afd6dd5789297d7151e2506e3", "General"],
   ["1337", "0d90c8723f3332ed834da1b26a72c84d18bd5de26aa5ae1424e2875d528c6b37", "leet"],
   ["sneed", "4afd1a29efebbcf9f134d8669f937969ca6fac04d64b68e3d493a9b974111cfe", "sneed"],
